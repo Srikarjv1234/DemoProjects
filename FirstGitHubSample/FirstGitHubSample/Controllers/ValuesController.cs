995831d468d4a042632e0f574086466c00878cb4
Venkata Srikar Jammulamadaka <Srikar.Jammulamadaka@valuemomentum.com>
2019-11-07 15:37:05 +0530

    Test

--- FirstGitHubSample/FirstGitHubSample/Controllers/ValuesController.cs
+++ FirstGitHubSample/FirstGitHubSample/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace FirstGitHubSample.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value5" };
+            return new string[] { "value3", "value5" };
         }
 
         // GET api/values/5
