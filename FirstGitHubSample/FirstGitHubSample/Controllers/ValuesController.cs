ed21eee32798ea81e91b82f4cd5dfcfbecc84c95
Venkata Srikar Jammulamadaka <Srikar.Jammulamadaka@valuemomentum.com>
2019-11-07 15:38:38 +0530

    update API METHOD

--- FirstGitHubSample/FirstGitHubSample/Controllers/ValuesController.cs
+++ FirstGitHubSample/FirstGitHubSample/Controllers/ValuesController.cs
@@ -18,7 +18,7 @@ namespace FirstGitHubSample.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return "value100";
         }
 
         // POST api/values
